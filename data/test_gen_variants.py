becc2348a1c33ad52f6bd45bf9a8808f51474512
Shishir Biyyala <Shishir.Biyyala@gmail.com>
2019-03-27 14:54:06 -0400

    Fully dockerized. Removes duplication, better restructuring. Consistent compose dependencies.

--- data/test_gen_variants.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from data.importVariants import gen_variants
-
-
-class TestGenVariants(TestCase):
-    def test_gen_variants(self):
-        gen_variants()
-        pass
