baca42a170ad9531eaf3d284ff7a0eaf37e61245
Shishir Biyyala <Shishir.Biyyala@lab49.com>
2019-03-27 21:24:02 -0400

    More cleanup, AG-grid instead of regular table.

--- services/variant_search/VariantSerializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from services.variant_search.models import Variant
-
-
-class VariantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Variant
-        fields = ('gene')
