83af087d02db9e0499852291ec875c330512a9a7
Shishir Biyyala <Shishir.Biyyala@gmail.com>
2019-04-20 12:29:51 -0400

    Remove commented out path

--- services/services/urls.py
+++ services/services/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include(router.urls))
     path('api/variant/', include('variant_search.urls'))
 
 ]
